fe997e62bfa70a2c9cbb6ea8b02ac70b2d0fadea
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-10-08 10:44:39 +0530

    added GP registers

--- 6502/cpu6502.go
+++ 6502/cpu6502.go
@@ -1,27 +1,57 @@
 package cpu6502
 
-type Bus16 interface {
-	Read(uint16) uint8
-	Write(uint16, uint8)
-}
-
 type cpu6502 struct {
-	Opcodes map[uint8]Instruction
+	operand uint8
+	Opcodes map[uint16]Instruction
 	PC      PC16
 	SP      SP8
+	A       GP8
+	X       GP8
+	Y       GP8
 	Bus     Bus16
 }
 
+//	Constructor function to create CPU6502
 func New(bus Bus16) *cpu6502 {
 	return &cpu6502{
 		Opcodes: load(),
 		PC:      NewPC(),
 		SP:      NewSP8(),
+		A:       NewGP8(),
+		X:       NewGP8(),
+		Y:       NewGP8(),
 		Bus:     bus,
 	}
 }
 
-func (c *cpu6502) Execute() {}
+//	check chan for new message or chan close
+func isClosed(close chan bool) bool {
+	select {
+	case <-close:
+		return true
+	default:
+		return false
+	}
+}
+
+/*
+ -> Check for cpu quit
+ -> Fetch Opcode from memory
+ -> Get instructon set with opcode
+ -> Increment Program Counter
+ -> Execute AddressMode func to get operand
+ -> Execute the instruction func with operand
+ -> Track no.of cycles that took for execution
+*/
+func (c *cpu6502) Execute(close chan bool) {
+	for !isClosed(close) {
+		opcode := c.fetch()
+		ins := c.Opcodes[uint16(opcode)]
+		cycle := ins.Cycle
+		cycle += ins.Mode(c)
+		cycle += ins.Ins(c)
+	}
+}
 
 func (c *cpu6502) read(add uint16) uint8 {
 	return c.Bus.Read(add)
@@ -29,4 +59,10 @@ func (c *cpu6502) read(add uint16) uint8 {
 
 func (c *cpu6502) write(add uint16, data uint8) {
 	c.Bus.Write(add, data)
+}
+
+func (c *cpu6502) fetch() uint8 {
+	data := c.read(c.PC.Get())
+	c.PC.Increment()
+	return data
 }
