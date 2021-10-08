bbe0deb23686b2f0fc50527b4f3338dec580ae70
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-10-08 10:45:20 +0530

    implemented GP8

---
+++ 6502/gp_registers.go
@@ -0,0 +1,18 @@
+package cpu6502
+
+type GP8Register struct {
+	data uint8
+}
+
+func NewGP8() GP8 {
+	return &GP8Register{
+		data: 0,
+	}
+}
+
+func (r *GP8Register) Get() uint8 {
+	return r.data
+}
+func (r *GP8Register) Set(data uint8) {
+	r.data = data
+}
