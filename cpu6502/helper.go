c1145d019580316b5ae39c0ccf85c063894f99e3
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-10-09 20:38:02 +0530

    changed visiblity

--- cpu6502/helper.go
+++ cpu6502/helper.go
@@ -1,7 +1,38 @@
 package cpu6502
 
-func littleEndianAddr(low, high uint8) uint16 {
+func LittleEndianAddr(low, high uint8) uint16 {
 	addr := uint16(low)
 	addr |= (uint16(high) << 8)
 	return addr
+}
+
+func IsZero(a uint8) bool {
+	return a == 0x00
+}
+
+func IsNev(a uint8) bool {
+	return a&0x80 != 0
+}
+
+func IsPos(a uint8) bool {
+	return a&0x80 == 0
+}
+
+func IsOverFlow(a, b uint8) bool {
+	c := a + b
+
+	if IsPos(a) && IsPos(b) && IsNev(c) {
+		return true
+	}
+
+	if IsNev(a) && IsNev(b) && IsPos(c) {
+		return true
+	}
+
+	return false
+}
+
+func IsCarry(a, b uint8) bool {
+	c := uint16(a) + uint16(b)
+	return c > 0x00ff
 }
--- cpu6502/helper_test.go
+++ cpu6502/helper_test.go
@@ -1,6 +1,7 @@
 package cpu6502
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -20,8 +21,109 @@ func TestLittleEndianAddr(t *testing.T) {
 		{low: 0xcd, high: 0xdc, addr: 0xdccd},
 		{low: 0x5f, high: 0x00, addr: 0x005f},
 	}
-	for _, test := range tests {
-		addr := littleEndianAddr(test.low, test.high)
-		assert.Equal(t, addr, test.addr)
+	for id, test := range tests {
+		testId := fmt.Sprintf("subtest-%v", id)
+		t.Run(testId, func(t *testing.T) {
+			got := LittleEndianAddr(test.low, test.high)
+			assert.Equal(t, test.addr, got)
+		})
 	}
+}
+
+func TestIsOverFlow(t *testing.T) {
+	tests := []struct {
+		a   uint8
+		b   uint8
+		exp bool
+	}{
+		{a: 0x50, b: 0x10, exp: false},
+		{a: 0x50, b: 0x50, exp: true},
+		{a: 0x50, b: 0x90, exp: false},
+		{a: 0x50, b: 0xd0, exp: false},
+		{a: 0xd0, b: 0x10, exp: false},
+		{a: 0xd0, b: 0x50, exp: false},
+		{a: 0xd0, b: 0x90, exp: true},
+		{a: 0xd0, b: 0xd0, exp: false},
+	}
+	for id, test := range tests {
+		testId := fmt.Sprintf("subtest-%v", id)
+		t.Run(testId, func(t *testing.T) {
+			got := IsOverFlow(test.a, test.b)
+			t.Logf("%X + %X -> %X", test.a, test.b, test.a+test.b)
+			assert.Equal(t, test.exp, got)
+		})
+	}
+}
+
+func TestIsPos(t *testing.T) {
+	tests := []struct {
+		a   uint8
+		exp bool
+	}{
+		{a: 0xff, exp: false},
+		{a: 0x0f, exp: true},
+		{a: 0xf0, exp: false},
+	}
+	for _, test := range tests {
+		got := IsPos(test.a)
+		assert.Equal(t, test.exp, got)
+	}
+}
+
+func TestIsNev(t *testing.T) {
+	tests := []struct {
+		a   uint8
+		exp bool
+	}{
+		{a: 0xff, exp: true},
+		{a: 0x0f, exp: false},
+		{a: 0xf0, exp: true},
+	}
+	for _, test := range tests {
+		got := IsNev(test.a)
+		assert.Equal(t, test.exp, got)
+	}
+}
+
+func TestIsZero(t *testing.T) {
+	tests := []struct {
+		a   uint8
+		exp bool
+	}{
+		{a: 0xff, exp: false},
+		{a: 0x0f, exp: false},
+		{a: 0xf0, exp: false},
+		{a: 0x00, exp: true},
+		{a: 0, exp: true},
+	}
+	for _, test := range tests {
+		got := IsZero(test.a)
+		assert.Equal(t, test.exp, got)
+	}
+}
+
+func TestIsCarry(t *testing.T) {
+	tests := []struct {
+		a   uint8
+		b   uint8
+		exp bool
+	}{
+		{a: 0x50, b: 0x10, exp: false},
+		{a: 0x50, b: 0x50, exp: false},
+		{a: 0x50, b: 0x90, exp: false},
+		{a: 0x50, b: 0xd0, exp: true},
+		{a: 0xd0, b: 0x10, exp: false},
+		{a: 0xd0, b: 0x50, exp: true},
+		{a: 0xd0, b: 0x90, exp: true},
+		{a: 0xd0, b: 0xd0, exp: true},
+	}
+	for id, test := range tests {
+		testId := fmt.Sprintf("subtest-%v", id)
+		t.Run(testId, func(t *testing.T) {
+			got := IsCarry(test.a, test.b)
+			t.Logf("%X + %X -> %X", test.a, test.b, test.a+test.b)
+			assert.Equal(t, test.exp, got)
+		})
+	}
+
 }
