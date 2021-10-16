c57bb9ada533c188d69c94936ba5b7d40ca53750
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-10-17 00:52:46 +0530

    updated

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	ram := []uint8{0xa9, 0x01, 0x8d, 0x00, 0x02, 0xa9, 0x05, 0x8d, 0x01, 0x02, 0xa9, 0x08, 0x8d, 0x02, 0x02}
 	bus16 := bus.NewBus16(ram)
-	cpu := cpu6502.New(bus16)
+	cpu := cpu6502.NewCpu(bus16, nil)
 	builder := opcode.NewOpcodeBuilder(cpu)
 	cpu.AttachOpcodeBuilder(builder)
 }
