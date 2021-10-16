35b149b51ad8708c93034f82d1ea690dd032b73b
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-10-17 00:28:13 +0530

    added cpu builder

---
+++ cpu6502/builder.go
@@ -0,0 +1,19 @@
+package cpu6502
+
+import (
+	"github.com/kabi175/6502/model"
+)
+
+//	Constructor function to create CPU6502
+func NewCpu(bus model.Bus16, deb model.Debugger) *Cpu6502 {
+	return &Cpu6502{
+		PC:   NewPC(),
+		SP:   NewSP8(),
+		Flag: NewFlagRegister(),
+		A:    NewGP8(),
+		X:    NewGP8(),
+		Y:    NewGP8(),
+		Bus:  bus,
+		deb:  deb,
+	}
+}
