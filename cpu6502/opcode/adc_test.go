4fa93db585638290234b20a0510d1f6e7125d27c
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-10-17 23:42:50 +0530

    added adc test

--- cpu6502/opcode/adc_test.go
+++ cpu6502/opcode/adc_test.go
@@ -8,8 +8,9 @@ import (
 
 func TestIMM(t *testing.T) {
 	tests := []test.Cputest{
+		// NON Decial Mode Test
 		{
-			Prg:  []uint8{0xa9, 0x10},
+			Prg:  []uint8{0x69, 0x10},
 			PC:   uint16(0x02),
 			SP:   uint8(0xFF),
 			Flag: uint8(0b00100000),
@@ -17,6 +18,56 @@ func TestIMM(t *testing.T) {
 			Y:    uint8(0x00),
 			A:    uint8(0x10),
 		},
+		{
+			Prg:  []uint8{0x38, 0x69, 0x10},
+			PC:   uint16(0x03),
+			SP:   uint8(0xFF),
+			Flag: uint8(0b00100000),
+			A:    uint8(0x11),
+		},
+		{
+			Prg:  []uint8{0x38, 0x69, 0xff},
+			PC:   uint16(0x03),
+			SP:   uint8(0xFF),
+			Flag: uint8(0b00100011),
+			A:    uint8(0x00),
+		},
+		// Decimal Mode Test
 	}
+	test.ProgramTest(t, tests)
+}
+
+func TestDecimalMode(t *testing.T) {
+	tests := []test.Cputest{
+		{
+			Prg:  []uint8{0xf8, 0x38, 0x69, 0x10},
+			PC:   uint16(0x04),
+			SP:   uint8(0xFF),
+			A:    uint8(0x11),
+			Flag: uint8(0b00101000),
+		},
+		{
+			Prg:  []uint8{0xf8, 0xa9, 0x10, 0x69, 0x10},
+			PC:   uint16(0x05),
+			SP:   uint8(0xFF),
+			A:    uint8(0x20),
+			Flag: uint8(0b00101000),
+		},
+		{
+			Prg:  []uint8{0xf8, 0xa9, 0x05, 0x69, 0x05},
+			PC:   uint16(0x05),
+			SP:   uint8(0xFF),
+			A:    uint8(0x10),
+			Flag: uint8(0b00101000),
+		},
+		{
+			Prg:  []uint8{0xf8, 0xa9, 0x50, 0x69, 0x50},
+			PC:   uint16(0x05),
+			SP:   uint8(0xFF),
+			A:    uint8(0x00),
+			Flag: uint8(0b01101011),
+		},
+	}
+
 	test.ProgramTest(t, tests)
 }
