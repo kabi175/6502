bb1fca97df668ceff31b77c6a6f26e27c6d1aae8
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-10-21 09:59:06 +0530

    re-org tests

--- cpu6502/opcode/accumulator_test.go
+++ cpu6502/opcode/accumulator_test.go
@@ -3,10 +3,11 @@ package opcode_test
 import (
 	"testing"
 
+	"github.com/kabi175/6502/cpu6502/opcode"
 	"github.com/kabi175/6502/cpu6502/test"
 )
 
-func TestIMM(t *testing.T) {
+func TestADC(t *testing.T) {
 	tests := []test.Cputest{
 		// NON Decial Mode Test
 		{
@@ -33,12 +34,6 @@ func TestIMM(t *testing.T) {
 			A:    uint8(0x00),
 		},
 		// Decimal Mode Test
-	}
-	test.ProgramTest(t, tests)
-}
-
-func TestDecimalMode(t *testing.T) {
-	tests := []test.Cputest{
 		{
 			Prg:  []uint8{0xf8, 0x38, 0x69, 0x10},
 			PC:   uint16(0x04),
@@ -67,13 +62,7 @@ func TestDecimalMode(t *testing.T) {
 			A:    uint8(0x00),
 			Flag: uint8(0b01101011),
 		},
-	}
-
-	test.ProgramTest(t, tests)
-}
-
-func TestZP(t *testing.T) {
-	tests := []test.Cputest{
+		// Zero Page
 		{
 			Prg:  []uint8{0xa9, 0xa2, 0x85, 0xff, 0xa9, 0x00, 0x65, 0xff},
 			A:    uint8(0xA2),
@@ -81,12 +70,7 @@ func TestZP(t *testing.T) {
 			PC:   uint16(0x08),
 			Flag: 0b10100000,
 		},
-	}
-	test.ProgramTest(t, tests)
-}
-
-func TestZPX(t *testing.T) {
-	tests := []test.Cputest{
+		// Zero Page X
 		{
 			Prg:  []uint8{0xa9, 0xa2, 0x85, 0xff, 0xa2, 0x05, 0xa9, 0x00, 0x75, 0xfa},
 			A:    uint8(0xA2),
@@ -95,12 +79,6 @@ func TestZPX(t *testing.T) {
 			PC:   uint16(0x0A),
 			Flag: 0b10100000,
 		},
-	}
-	test.ProgramTest(t, tests)
-}
-
-func TestABS(t *testing.T) {
-	tests := []test.Cputest{
 		{
 			Prg:  []uint8{0xa9, 0xa2, 0x8d, 0x00, 0x10, 0xa9, 000, 0x6d, 0x00, 0x10},
 			A:    uint8(0xA2),
@@ -108,11 +86,6 @@ func TestABS(t *testing.T) {
 			PC:   uint16(0x0A),
 			Flag: 0b10100000,
 		},
-	}
-	test.ProgramTest(t, tests)
-}
-func TestABX(t *testing.T) {
-	tests := []test.Cputest{
 		{
 			Prg:  []uint8{0xa9, 0xa2, 0x8d, 0x05, 0x10, 0xa2, 0x05, 0xa9, 0x00, 0x7d, 0x00, 0x10},
 			A:    uint8(0xA2),
@@ -121,11 +94,6 @@ func TestABX(t *testing.T) {
 			PC:   uint16(0x0C),
 			Flag: 0b10100000,
 		},
-	}
-	test.ProgramTest(t, tests)
-}
-func TestABY(t *testing.T) {
-	tests := []test.Cputest{
 		{
 			Prg:  []uint8{0xa9, 0xa2, 0x8d, 0x05, 0x10, 0xa0, 0x05, 0xa9, 0x00, 0x79, 0x00, 0x10},
 			A:    uint8(0xA2),
@@ -133,6 +101,92 @@ func TestABY(t *testing.T) {
 			SP:   uint8(0xFF),
 			PC:   uint16(0x0C),
 			Flag: 0b10100000,
+		},
+	}
+	test.ProgramTest(t, tests)
+}
+
+func TestAND(t *testing.T) {
+	tests := []test.Cputest{
+		{
+			Prg:  []uint8{0xa9, 0x2a, 0x29, 0x32},
+			SP:   uint8(0xff),
+			PC:   uint16(0x04),
+			A:    uint8(0x22),
+			Flag: 0b00100000,
+		},
+		{
+			Prg:  []uint8{0xa9, 0x2a, 0x29, 0x00},
+			SP:   uint8(0xff),
+			PC:   uint16(0x04),
+			A:    uint8(0x00),
+			Flag: 0b00100010,
+		},
+		{
+			Prg:  []uint8{0xa9, 0xff, 0x29, 0xff},
+			SP:   uint8(0xff),
+			PC:   uint16(0x04),
+			A:    uint8(0xff),
+			Flag: 0b10100000,
+		},
+		{
+			Prg:  []uint8{0xa9, 0xa4, 0x85, 0xf0, 0xa9, 0xc6, 0x25, 0xf0},
+			A:    uint8(0x84),
+			SP:   uint8(0xff),
+			PC:   uint16(0x08),
+			Flag: 0b10100000,
+		},
+	}
+	test.ProgramTest(t, tests)
+
+	// Zero page
+	test.OpcodeTest(t, 0x25, opcode.AND, opcode.ZPA)
+	test.OpcodeTest(t, 0x35, opcode.AND, opcode.ZPX)
+
+	// Absolute Mode
+	test.OpcodeTest(t, 0x2D, opcode.AND, opcode.ABS)
+	test.OpcodeTest(t, 0x3D, opcode.AND, opcode.ABX)
+	test.OpcodeTest(t, 0x39, opcode.AND, opcode.ABY)
+	// Relative Indexed
+	test.OpcodeTest(t, 0x21, opcode.AND, opcode.IDX)
+	test.OpcodeTest(t, 0x31, opcode.AND, opcode.IDY)
+}
+
+func Test_ASL(t *testing.T) {
+	tests := []test.Cputest{
+		{
+			Prg:  []uint8{0xa9, 0x34, 0x0a},
+			A:    uint8(0x68),
+			PC:   uint16(0x03),
+			SP:   uint8(0xff),
+			Flag: 0b00100000,
+		},
+		{
+			Prg:  []uint8{0xa9, 0x43, 0x85, 0xf0, 0x06, 0xf0, 0xa5, 0xf0},
+			A:    uint8(0x86),
+			PC:   uint16(0x08),
+			SP:   uint8(0xff),
+			Flag: 0b10100000,
+		},
+	}
+	test.ProgramTest(t, tests)
+}
+
+func TestBIT(t *testing.T) {
+	tests := []test.Cputest{
+		{
+			Prg:  []uint8{0xa9, 0xf3, 0x85, 0xf0, 0x24, 0xf0},
+			A:    0xF3,
+			SP:   0xFF,
+			PC:   0x06,
+			Flag: 0b11100000,
+		},
+		{
+			Prg:  []uint8{0xa9, 0xa3, 0x85, 0xf0, 0xa9, 0x00, 0x24, 0xf0},
+			A:    0x0,
+			SP:   0xFF,
+			PC:   0x08,
+			Flag: 0b10100010,
 		},
 	}
 	test.ProgramTest(t, tests)
